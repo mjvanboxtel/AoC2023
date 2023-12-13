8df784faa9fe7195753dbc008ed0c745aa663c10
Matthew Van Boxtel <matthew.vanboxtel@arcticwolf.com>
2023-12-13 00:00:04 -0500

    day11 problem1 and problem2

---
+++ day11/problem1/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"math"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	defer log.Printf("Execution time %s", time.Since(start))
+
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		log.Println(err)
+	}
+	defer readFile.Close()
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	var galaxies [][]int
+
+	var universeWidth int
+	var idx int
+	for fileScanner.Scan() {
+		lineRunes := []rune(fileScanner.Text())
+		universeWidth = len(lineRunes)
+		currentGalaxies := findGalaxies(lineRunes, idx)
+		galaxies = append(galaxies, currentGalaxies...)
+		idx++
+	}
+
+	galaxies = expandUniverse(galaxies, universeWidth, idx)
+
+	var output int
+
+	for i := 0; i < len(galaxies); i++ {
+		for j := 0; j < len(galaxies); j++ {
+			distance := findDistance(galaxies[i], galaxies[j])
+			if distance == 0 {
+				continue
+			}
+			output += distance
+		}
+	}
+
+	if output%2 == 0 {
+		output = output / 2
+	} else {
+		output = int(math.Ceil(float64(output / 2)))
+	}
+
+	log.Printf("Value: %d\n", output)
+}
+
+func findDistance(g1 []int, g2 []int) int {
+	dY := math.Abs(float64(g1[0]) - float64(g2[0]))
+	dX := math.Abs(float64(g1[1]) - float64(g2[1]))
+	return int(dY + dX)
+}
+
+func findGalaxies(line []rune, row int) [][]int {
+	var galaxies [][]int
+	for i := 0; i < len(line); i++ {
+		if line[i] == '#' {
+			galaxies = append(galaxies, []int{i, row})
+		}
+	}
+	return galaxies
+}
+
+func expandUniverse(galaxies [][]int, initialWidth int, initialLength int) [][]int {
+	rowGalaxies := make([]bool, initialWidth)
+	columnGalaxies := make([]bool, initialLength)
+
+	for i := 0; i < len(galaxies); i++ {
+		rowGalaxies[galaxies[i][1]] = true
+		columnGalaxies[galaxies[i][0]] = true
+	}
+
+	for i := 0; i < len(galaxies); i++ {
+		rowIncrease := getExpansionCount(columnGalaxies, galaxies[i][0])
+		colIncrease := getExpansionCount(rowGalaxies, galaxies[i][1])
+		galaxies[i][0] += rowIncrease
+		galaxies[i][1] += colIncrease
+	}
+
+	return galaxies
+}
+
+func getExpansionCount(galaxyExist []bool, end int) int {
+	var out int
+	for i := 0; i < end; i++ {
+		if galaxyExist[i] == false {
+			out++
+		}
+	}
+	return out
+}
